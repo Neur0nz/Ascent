59f71c2717bd26d0e9d69d5968465be2c7450d2c
Neur0nz <nadavprr@gmail.com>
2025-11-16 14:34:53 +0200

    ff

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -35,6 +35,18 @@ export default defineConfig(({ mode }) => {
               type: 'image/svg+xml',
               purpose: 'any maskable',
             },
+            {
+              src: '/icons/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+            {
+              src: '/icons/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
           ],
         },
         injectManifest: {
